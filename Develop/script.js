f86d599a43b0c86ccf05e7bf6330716802fa7660
Tyler Smith <mrtbird539@gmail.com>
2021-07-11 16:57:36 -0400

    Finished JS Code. App works

--- Develop/script.js
+++ Develop/script.js
@@ -1,59 +1,69 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
-function getLowerCase () {
-  return String.fromCharCode.Math.floor((Math.random() * 26)+ 97);
-}
+let numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
-function getUpperCase() {
-  return String.fromCharCode.Math.floor((Math.random() * 26) + 65);
-}
-
-function getNumber() {
-  return String.fromCharCode.Math.floor((Math.random() * 10) + 48);
-}
-
-function getSymbol() {
-  let symbols = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-  let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
-    return randomSymbol;
-}
 
 function getInfo() {
   let passLength = prompt("How long do you want your password? (enter between 8 and 128)");
-  
-  if ((passLength < 128) && (passLength > 8)) {
+
+  if ((!passLength < 128) && (!passLength > 8)) {
+    alert("Enter a valid length")
+    return
+  }
   let askNumbers = confirm("Do you wish to include numbers?");
   let askLower = confirm("Do you wish to include lower case letters?");
   let askUpper = confirm("Do you wish to include upper case letters?");
   let askSymbol = confirm("Do you wish to include symbols?");
-  }
-  else{
-    alert("Enter a valid length")
-  }
 
-  if ((!askNumbers) && (!askLower) && (!askSymbol) && (!askUpper)) {alert("Please select at least one type.")}
+  if ((!askNumbers) && (!askLower) && (!askSymbol) && (!askUpper)) { alert("Please select at least one type.") }
 
   let answers = {
-  length: passLength,
-  numbers: askNumbers,
-  lower: askLower,
-  upper: askUpper,
-  symbol: askSymbol
+    length: passLength,
+    numbers: askNumbers,
+    lower: askLower,
+    upper: askUpper,
+    symbol: askSymbol
   }
   return answers;
 }
 
-generatePassword(answers);
+function generatePassword() {
+  let parameters = getInfo();
+  let password = [];
 
-
-getInfo()
+  if (parameters.numbers) {
+    for (let i of numbers)
+      password.push(i);
+  }
+  if (parameters.lower) {
+    for (let i of lowerCase)
+      password.push(i);
+  }
+  if (parameters.upper) {
+    for (let i of upperCase)
+      password.push(i);
+  }
+  if (parameters.symbol) {
+    for (let i of special)
+      password.push(i);
+  }
+  console.log(password)
+  let randomPassword = ""
+  for (let i = 0; i < parameters.length; i++) {
+    randomPassword += password[Math.floor(Math.random() * password.length)];
+  }
+  console.log(randomPassword)
+  return randomPassword
+}
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  let randomPassword = generatePassword();
+  let passwordText = document.querySelector("#password");
+  passwordText.value = randomPassword;
 
 }
 
